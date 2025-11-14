bb2661304063b5499e2e123ae8fcdb3e3b44ab65
Mettzy_ <alarsabandy7@gmail.com>
2025-11-14 15:26:34 +0700

    risk_local.py

--- core/risk_local.py
+++ core/risk_local.py
@@ -1,1 +1,17 @@
+KEYWORDS = {
+    "scam": 1.0,
+    "fraud": 1.0,
+    "hack": 0.9,
+    "exploit": 0.9,
+    "phish": 0.9,
+    "airdrop": -0.6,
+    "reward": -0.5
+}
 
+def local_score(text):
+    t = text.lower()
+    score = 0
+    for k, w in KEYWORDS.items():
+        if k in t:
+            score += w
+    return round(score, 2)
